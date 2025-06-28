5162a2f2ca605e7d203f44b7345c1ec19484ad00
Fercan Şen <fercan.sen@p.tech>
2025-06-28 22:40:44 +0300

    Add Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,24 +1,43 @@
+"use client";
+
 import Link from "next/link";
 import { sidebarMenuItems } from "./sidebarMenu";
+import { cn } from "@/lib/utils";
 
-const Sidebar = () => {
-  return (
-    <aside className="w-64 bg-white shadow-md h-screen p-4">
-      <h2 className="text-xl font-bold mb-6">Timeke</h2>
-      <nav className="flex flex-col gap-4">
-        {sidebarMenuItems.map(({ title, href, icon: Icon }) => (
-          <Link
-            key={href}
-            href={href}
-            className="flex items-center gap-2 p-2 hover:bg-gray-200 rounded"
-          >
-            <Icon className="w-5 h-5" />
-            <span>{title}</span>
-          </Link>
-        ))}
-      </nav>
-    </aside>
+interface SidebarProps {
+  isOpen: boolean;
+  toggleSidebar: () => void;
+}
+
+export default function Sidebar({ isOpen, toggleSidebar }: SidebarProps) {
+  const sidebarClass = cn(
+    "fixed md:static top-16 left-0 h-[calc(100vh-4rem)] bg-secondary shadow-md p-4 transition-all duration-300 w-screen md:w-64",
+    {
+      "translate-x-0 opacity-100": isOpen,
+      "-translate-x-full opacity-0 pointer-events-none": !isOpen,
+      "md:translate-x-0 md:opacity-100 md:pointer-events-auto": true,
+    }
   );
-};
 
-export default Sidebar;
+  return (
+    <>
+      <div className={sidebarClass}>
+        <nav className="flex flex-col gap-4">
+          {sidebarMenuItems.map(({ title, href, icon: Icon }) => (
+            <Link
+              key={href}
+              href={href}
+              className="flex items-center gap-2 p-2 hover:bg-gray-200 rounded"
+              onClick={() => {
+                if (isOpen) toggleSidebar();
+              }}
+            >
+              <Icon className="w-5 h-5" />
+              <span>{title}</span>
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </>
+  );
+}
